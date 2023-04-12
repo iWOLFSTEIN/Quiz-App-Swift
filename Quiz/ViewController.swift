28d1f7172b328fa76a64d1119eebdc8f7b3ba7ed
Talha Ashraf <talha.ashraf@brainxtech.com>
2023-04-12 15:55:22 +0500

    quiz app ui created

--- Quiz/ViewController.swift
+++ Quiz/ViewController.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nextButton: UIButton!
+    @IBOutlet weak var checkButton: UIButton!
+    @IBOutlet weak var questionAnswerView: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        questionAnswerView.layer.cornerRadius = 10
+        checkButton.layer.cornerRadius = 10
+        nextButton.layer.cornerRadius = 10
     }
 
-
+    @IBAction func checkAnswer(_ sender: UIButton) {
+    }
+    
+    @IBAction func nextQuestion(_ sender: Any) {
+    }
 }
 
